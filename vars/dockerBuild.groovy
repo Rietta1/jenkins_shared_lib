910e99a1afa27d53597e702b4d0bafe3b5b14c40
Rietta1 <riettabusiness@gmail.com>
2023-07-27 16:41:40 +0100

    docker build for ECR shared library added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -5,7 +5,7 @@
 //      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
 //      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
 //     """
-// }
+// } 
 
 def call(String aws_account_id, String region, String ecr_repoName){
     
